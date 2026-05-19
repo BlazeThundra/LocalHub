9942d4c4e288030a51a0a9c870bfc6b3503feef2
Blaze <aaronmichaeldavis@outlook.com>
2026-05-19 15:54:46 -0500

    back code (knob)

--- Assets/Games/Hub/Scripts/HubManager.cs
+++ Assets/Games/Hub/Scripts/HubManager.cs
@@ -1,10 +1,43 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Hub
 {
 public class HubManager : MonoBehaviour
 {
+ Image knob;
+ private float holdTimer = 0f;
+ [SerializeField] private float requiredHoldTime = 2f;
+ 
+ void Awake()
+ {
+  knob = GetComponentInChildren<Image>();
+  holdTimer = 0f;
+  DontDestroyOnLoad(gameObject);
+ }
+
+ void Update()
+ {
+  if(Input.GetKey(KeyCode.Escape))
+  {
+   holdTimer += Time.deltaTime;
+   knob.fillAmount = holdTimer/requiredHoldTime;
+
+   if(holdTimer >= requiredHoldTime)
+   {
+    holdTimer = 0f;
+    LoadScene("Hub");
+   }
+  }
+
+  if(Input.GetKeyUp(KeyCode.Escape))
+  {
+   holdTimer = 0f;
+   knob.fillAmount = holdTimer/requiredHoldTime;
+  }
+ }
+
  public void LoadScene(string sceneName)
  {
   SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
